b8ac4244e660d43b63aea2a755622e433aa78b19
2pioseem8 <2pioseem8@gmail.com>
2016-11-23 22:13:18 +0100

    commit6

--- Nowy dokument tekstowy6.py
+++ Nowy dokument tekstowy6.py
@@ -1,49 +1,49 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# importujemy funkcje z modu³u ocenyfun zapisanego w pliku ocenyfun.py
+# importujemy funkcje z moduÅ‚u ocenyfun zapisanego w pliku ocenyfun.py
 from ocenyfun import drukuj
 from ocenyfun import srednia
 from ocenyfun import mediana
 from ocenyfun import odchylenie
 
 przedmioty = set(['polski', 'angielski'])  # definicja zbioru
-drukuj(przedmioty, "Lista przedmiotów zawiera: ")
+drukuj(przedmioty, "Lista przedmiotÃ³w zawiera: ")
 
-print "\nAby przerwaæ wprowadzanie przedmiotów, naciœnij Enter."
+print "\nAby przerwaÄ‡ wprowadzanie przedmiotÃ³w, naciÅ›nij Enter."
 while True:
-    przedmiot = raw_input("Podaj nazwê przedmiotu: ")
+    przedmiot = raw_input("Podaj nazwÄ™ przedmiotu: ")
     if len(przedmiot):
         if przedmiot in przedmioty:  # czy przedmiot jest w zbiorze?
-            print "Ten przedmiot ju¿ mamy :-)"
+            print "Ten przedmiot juÅ¼ mamy :-)"
         przedmioty.add(przedmiot)  # dodaj przedmiot do zbioru
     else:
         drukuj(przedmioty, "\nTwoje przedmioty: ")
-        przedmiot = raw_input("\nZ którego przedmiotu wprowadzisz oceny? ")
-        if przedmiot not in przedmioty:  # je¿eli przedmiotu nie ma w zbiorze
-            print "Brak takiego przedmiotu, mo¿esz go dodaæ."
+        przedmiot = raw_input("\nZ ktÃ³rego przedmiotu wprowadzisz oceny? ")
+        if przedmiot not in przedmioty:  # jeÅ¼eli przedmiotu nie ma w zbiorze
+            print "Brak takiego przedmiotu, moÅ¼esz go dodaÄ‡."
         else:
-            break  # wyjœcie z pêtli
+            break  
 
 oceny = []  # pusta lista ocen
-ocena = None  # zmienna steruj¹ca pêtl¹ i do pobierania ocen
-print "\nAby przerwaæ wprowadzanie ocen, podaj 0 (zero)."
+ocena = None  # zmienna sterujÄ…ca pÄ™tlÄ… i do pobierania ocen
+print "\nAby przerwaÄ‡ wprowadzanie ocen, podaj 0 (zero)."
 
 while not ocena:
     try:
-        ocena = int(raw_input("Podaj ocenê (1-6): "))
+        ocena = int(raw_input("Podaj ocenÄ™ (1-6): "))
         if (ocena > 0 and ocena < 7):
             oceny.append(float(ocena))
         elif ocena == 0:
             break
         else:
-            print "B³êdna ocena."
+            print "BÅ‚Ä™dna ocena."
         ocena = None
     except ValueError:
-        print "B³êdne dane!"
+        print "BÅ‚Ä™dne dane!"
 
 drukuj(oceny, przedmiot.capitalize() + " - wprowadzone oceny: ")
-s = srednia(oceny)  # wywo³anie funkcji z modu³u ocenyfun
-m = mediana(oceny)  # wywo³anie funkcji z modu³u ocenyfun
-o = odchylenie(oceny, s)  # wywo³anie funkcji z modu³u ocenyfun
-print "\nŒrednia: {0:5.2f}\nMediana: {1:5.2f}\nOdchylenie: {2:5.2f}".format(s, m, o)
+s = srednia(oceny)  # wywoÅ‚anie funkcji z moduÅ‚u ocenyfun
+m = mediana(oceny)  # wywoÅ‚anie funkcji z moduÅ‚u ocenyfun
+o = odchylenie(oceny, s)  # wywoÅ‚anie funkcji z moduÅ‚u ocenyfun
+print "\nÅšrednia: {0:5.2f}\nMediana: {1:5.2f}\nOdchylenie: {2:5.2f}".format(s, m, o)
